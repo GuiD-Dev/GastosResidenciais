6c7887b402c7d83fb134dbf73375c55751662183
Guilherme <guilhermed.dev@gmail.com>
2025-12-22 18:31:22 -0300

    refactor: add explicit operators to cast person entites to dtos, and vice versa

--- GastosResidenciais.WebApi/GastosResidenciais.WebAPI.API/DTOs/PersontDto.cs
+++ GastosResidenciais.WebApi/GastosResidenciais.WebAPI.API/DTOs/PersontDto.cs
@@ -1,7 +1,37 @@
+using GastosResidenciais.WebApi.Domain.Entities;
+
 namespace GastosResidenciais.WebApi.API.DTOs;
 
 public class PersonDto
 {
     public string Name { get; set; }
     public int Age { get; set; }
+
+    public static explicit operator Person(PersonDto dto)
+    {
+        return new Person
+        {
+            Name = dto.Name,
+            Age = dto.Age,
+        };
+    }
+
+    public static explicit operator PersonDto(Person entity)
+    {
+        return new PersonDto
+        {
+            Name = entity.Name,
+            Age = entity.Age,
+            Transactions = entity.Transactions
+                .Select(transaction => new TransactionDto
+                {
+                    Description = transaction.Description,
+                    Value = transaction.Value,
+                    Type = transaction.Type,
+                }),
+            Recipes = entity.Recipes,
+            Expenses = entity.Expenses,
+            Balance = entity.Balance,
+        };
+    }
 }
