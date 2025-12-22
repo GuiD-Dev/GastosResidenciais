76a36bebb36616a833c5ee0f0934129a922241eb
Guilherme <guilhermed.dev@gmail.com>
2025-12-22 18:30:30 -0300

    refactor: add explicit operators to cast category entites to dtos, and vice versa

--- GastosResidenciais.WebApi/GastosResidenciais.WebAPI.API/DTOs/CategorytDto.cs
+++ GastosResidenciais.WebApi/GastosResidenciais.WebAPI.API/DTOs/CategorytDto.cs
@@ -1,3 +1,4 @@
+using GastosResidenciais.WebApi.Domain.Entities;
 using GastosResidenciais.WebApi.Domain.Enums;
 
 namespace GastosResidenciais.WebApi.API.DTOs;
@@ -6,4 +7,22 @@ public class CategoryDto
 {
     public string Description { get; set; }
     public CategoryPurpose Purpose { get; set; }
+
+    public static explicit operator Category(CategoryDto dto)
+    {
+        return new Category
+        {
+            Description = dto.Description,
+            Purpose = dto.Purpose,
+        };
+    }
+
+    public static explicit operator CategoryDto(Category entity)
+    {
+        return new CategoryDto
+        {
+            Description = entity.Description,
+            Purpose = entity.Purpose,
+        };
+    }
 }
