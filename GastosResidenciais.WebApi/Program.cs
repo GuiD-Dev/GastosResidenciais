e9c508e9905c06428bf2112c322fb82304097672
Guilherme <guilhermed.dev@gmail.com>
2025-12-23 09:47:42 -0300

    refactor: add cors middleware

--- GastosResidenciais.WebApi/Program.cs
+++ GastosResidenciais.WebApi/Program.cs
@@ -10,6 +10,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()
+    );
+});
+
 builder.Services.AddControllers();
 
 builder.Services.AddScoped<IPersonService, PersonService>();
@@ -33,6 +40,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+app.UseCors();
 app.UseHttpsRedirection();
 app.MapControllers();
 
