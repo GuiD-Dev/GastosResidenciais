9268d73eef43a2b72feba9447dda85578eaa31da
Guilherme <guilhermed.dev@gmail.com>
2025-12-29 22:24:54 -0300

    refactor: change startup project to API and add CrossCutting project to centralize dependencies

---
+++ HomeFinances.WebApi/HomeFinances.WebApi.API/Program.cs
@@ -0,0 +1,31 @@
+using HomeFinances.WebApi.CrossCutting;
+using HomeFinances.WebApi.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()
+    );
+});
+
+builder.Services.AddControllers();
+
+builder.Services.AddDependencies(builder.Configuration);
+
+var app = builder.Build();
+
+// Garantir que as migrations sejam aplicadas automaticamente ao iniciar a aplicação.
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<PgSqlDbContext>();
+    db.Database.Migrate();
+}
+
+app.UseCors();
+app.UseHttpsRedirection();
+app.MapControllers();
+
+app.Run();
---
+++ HomeFinances.WebApi/HomeFinances.WebApi.CrossCutting/DependencyInjection.cs
@@ -0,0 +1,30 @@
+﻿using HomeFinances.WebApi.Application.Interfaces;
+using HomeFinances.WebApi.Application.Services;
+using HomeFinances.WebApi.Infrastructure.Contexts;
+using HomeFinances.WebApi.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HomeFinances.WebApi.CrossCutting;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddDependencies(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<PgSqlDbContext>(options =>
+            options.UseNpgsql(configuration["PgSQlConnection:PgSQlConnectionString"])
+        );
+
+        services.AddScoped<IPersonService, PersonService>();
+        services.AddScoped<IPersonRepository, PersonRepository>();
+
+        services.AddScoped<ICategoryService, CategoryService>();
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+        services.AddScoped<ITransactionService, TransactionService>();
+        services.AddScoped<ITransactionRepository, TransactionRepository>();
+
+        return services;
+    }
+}
